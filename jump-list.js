392c874e3ae65c68df831310f669a0590eb4bab0
MoeWhite19 <MoeWhite19@163.com>
2017-04-28 20:11:30 +0800

    bcol

---
+++ jump-list.js
@@ -0,0 +1,23 @@
+
+var jumplist=[
+ {
+	name:'dm',
+	start:'http://www.',
+	end:'.cn'
+ },
+ {
+	name:'about',
+	start:'http://www.whiteg.cn/',
+	end:'index.html'
+ },
+ {
+	 name:'p',
+	 start:'http://whiteg.cn/pan/index.php?share/folder&user=1&sid=',
+	 end:''
+ },
+ {
+	 name:'c',
+	 start:'http://whiteg.cn/chat/',
+	 end:'.html'
+ }
+];
